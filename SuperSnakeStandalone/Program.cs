2928f4b567f3e4e5e83ce78e0743e035a080f8ca
Chisaka <a1502020@sendai-nct.jp>
2016-02-02 11:43:21 +0900

    プロジェクト SuperSnakeStandalone を作成

---
+++ SuperSnakeStandalone/Program.cs
@@ -0,0 +1,26 @@
+﻿using DxLibDLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperSnakeStandalone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DX.ChangeWindowMode(DX.TRUE);
+            DX.DxLib_Init();
+            DX.SetDrawScreen(DX.DX_SCREEN_BACK);
+
+            while (DX.ProcessMessage() == 0)
+            {
+                DX.ScreenFlip();
+            }
+
+            DX.DxLib_End();
+        }
+    }
+}
