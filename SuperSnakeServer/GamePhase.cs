c770e05d1d12876e7eb8840780f15fb14eee6905
Chisaka <a1502020@sendai-nct.jp>
2017-01-24 21:25:12 +0900

    サーバー側で全プレイヤーの色と名前を表示するようにした

--- SuperSnakeServer/GamePhase.cs
+++ SuperSnakeServer/GamePhase.cs
@@ -99,6 +99,19 @@ namespace SuperSnakeServer
             // 描画
             DX.DrawFillBox(0, 0, 640, 480, DX.GetColor(0, 0, 0));
 
+            // 各プレイヤーの色と名前を表示
+            for (int i = 0; i < playersCount; i++)
+            {
+                var x = Math.Max(440, 4 + gameStateDrawer.BasePos.X + gameStateDrawer.CellSize.Width * game.State.Field.Width);
+                var y = 4 + 20 * i;
+                var strNum = i.ToString();
+                var dx = DX.GetDrawStringWidth(strNum, strNum.Length);
+                DX.DrawFillBox(x - 2, y - 2, x + dx + 2, y + 20, DX.GetColor(255, 255, 255));
+                var col = game.State.Players[i].Color;
+                DX.DrawString(x, y, strNum, DX.GetColor(col.R, col.G, col.B));
+                DX.DrawString(x + dx + 4, y, game.State.Players[i].Name, DX.GetColor(255, 255, 255));
+            }
+
             // 各プレイヤーの状態を表示
             var tbl = new int[playersCount, 4];
             for (int i = 0; i < playersCount; i++)
--- SuperSnakeServer/ResultPhase.cs
+++ SuperSnakeServer/ResultPhase.cs
@@ -22,7 +22,18 @@ namespace SuperSnakeServer
             // 描画
             DX.DrawFillBox(0, 0, 640, 480, DX.GetColor(0, 0, 0));
 
-            gameStateDrawer.Draw(game.State);
+            // 各プレイヤーの色と名前を表示
+            for (int i = 0; i < game.State.PlayersCount; i++)
+            {
+                var x = Math.Max(440, 4 + gameStateDrawer.BasePos.X + gameStateDrawer.CellSize.Width * game.State.Field.Width);
+                var y = 4 + 20 * i;
+                var strNum = i.ToString();
+                var dx = DX.GetDrawStringWidth(strNum, strNum.Length);
+                DX.DrawFillBox(x - 2, y - 2, x + dx + 2, y + 20, DX.GetColor(255, 255, 255));
+                var col = game.State.Players[i].Color;
+                DX.DrawString(x, y, strNum, DX.GetColor(col.R, col.G, col.B));
+                DX.DrawString(x + dx + 4, y, game.State.Players[i].Name, DX.GetColor(255, 255, 255));
+            }
 
             var str = "引き分け";
             for (int playerNum = 0; playerNum < game.State.PlayersCount; playerNum++)
@@ -34,6 +45,8 @@ namespace SuperSnakeServer
                 }
             }
             DX.DrawString(2, 2, str, DX.GetColor(255, 255, 255));
+
+            gameStateDrawer.Draw(game.State);
 
             return this;
         }
